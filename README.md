275808a7ccf2c386552c507d2f22fb3a22789c17
mazarini <pascal.riva@gmail.com>
2023-02-24 12:18:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# mazarini/design-bundle
+
+Help for the design of a symfony website.
---
+++ tests/bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of mazarini/design-bundle.
+ *
+ * mazarini/design-bundle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mazarini/design-bundle is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with mazarini/design-bundle. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @package mazarini/design-bundle
+ */
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
+    require dirname(__DIR__).'/config/bootstrap.php';
+} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0);
+}
